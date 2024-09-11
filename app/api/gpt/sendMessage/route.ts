a6cca21fe6f7606f1cd84b82e17f79bdebc9d6ed
Haron229 <artemevich.artemiy@gmail.com>
2024-09-11 14:21:22 +0400

    upd

--- app/api/gpt/sendMessage/route.ts
+++ app/api/gpt/sendMessage/route.ts
@@ -22,8 +22,10 @@ export const POST = async (req: NextRequest) => {
     });
 
     const result = ChatCompletionResponseSchema.safeParse(completion);
+    console.log(JSON.stringify(completion.choices[0]));
+    console.log("");
     console.log(JSON.stringify(result));
-	
+
     return result.success
       ? new NextResponse(JSON.stringify(result.data.choices[0]), {
           status: 200,
