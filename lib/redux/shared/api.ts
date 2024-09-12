df4a4f5caa631fa128da38d3df651864cbbf6297
Haron229 <artemevich.artemiy@gmail.com>
2024-09-13 02:57:01 +0400

    upd

--- lib/redux/shared/api.ts
+++ lib/redux/shared/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://themixreality.com/api" }),
   endpoints: () => ({}),
 });
